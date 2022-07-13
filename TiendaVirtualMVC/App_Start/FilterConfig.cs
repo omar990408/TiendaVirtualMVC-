228e6aeeb1d879db22fa2c3af38ed0959e00b5c2
elegant00 <andre@DESKTOP-UPDSHVD>
2022-07-13 01:42:32 -0500

    dependencies

--- TiendaVirtualMVC/App_Start/FilterConfig.cs
+++ TiendaVirtualMVC/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace TiendaVirtualMVC
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new Filters.Filtro_VERIFICA_SESSION());
+           // filters.Add(new Filters.Filtro_VERIFICA_SESSION());
         }
     }
 }
